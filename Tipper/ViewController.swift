9e2cd08db9d452c6bb3a81712f7643d5986cf7a2
Akshat Goyal <akshatgoyal@pal-nat184-025-071.itap.purdue.edu>
2015-12-04 00:31:56 -0500

    Initial Commit

---
+++ Tipper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tipper
+//
+//  Created by Akshat Goyal on 12/4/15.
+//  Copyright © 2015 Akshat Goyal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
